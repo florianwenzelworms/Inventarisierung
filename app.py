8c8fb39ae958305321723497d48c4038d2d886a7
wenzelf <florian.wenzel@worms.de>
2024-08-06 15:42:22 +0200

    Test of other code from net

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def logout():
 
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=3000, debug=True)
-    app.run(host='0.0.0.0', port=3000, debug=True, ssl_context="adhoc")
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    #app.run(host='0.0.0.0', port=3000, debug=True, ssl_context="adhoc")

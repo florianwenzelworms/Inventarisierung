bdb239dfd8d60658118c9e618ed3ad5a88bfae06
wenzelf <florian.wenzel@worms.de>
2024-08-13 14:44:55 +0200

    Changed user authentification to LDAP removed old user system removed user table added LDAP sync added LDAP user creation added function to remove scanned codes

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime, timezone
 from forms import LoginForm
 import json
 import base64
+import ldap3
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'e79b9847144221ba4e85df9dd483a3e5'
@@ -14,17 +15,72 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
+# LDAP Settings
+LDAP_USER = "anmelde_dom\\readad"
+LDAP_PASS = "stwo1515.."
+LDAP_SERVER = "ldap://2.1.10.245"
+AD_DOMAIN = "ANMELDE_DOM"
+SEARCH_BASE = "CN=Users,DC=stadt,DC=worms"
+
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+        self.cn = ''
+        self.mail = ''
+        self.department = ''
+        self.groups = []
+
 
 @login_manager.user_loader
-def load_user(user_id):
-    return db.session.query(User).filter(User.id == user_id).first()
+def load_user(uid):
+    user = User(uid)
+    if user:
+        user.cn, user.mail, user.department, user.groups = get_user_data(uid)
+    return user
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+def authenticate_ldap(username, password):
+    """
+    Check authentication of user against AD with LDAP
+    :param username: Username
+    :param password: Password
+    :return: True is authentication is successful, else False
+    """
+    server = ldap3.Server(LDAP_SERVER, use_ssl=True, get_info=ldap3.ALL)
+
+    try:
+        with ldap3.Connection(server,
+                              user=f'{AD_DOMAIN}\\{username}',
+                              password=password,
+                              authentication=ldap3.SIMPLE,
+                              check_names=True,
+                              raise_exceptions=True) as conn:
+            if conn.bind():
+                print("Authentication successful")
+                user = User(username)
+                return user
+    except Exception as e:
+        print(f"LDAP authentication failed: {e}")
+    return False
+
+
+def get_user_data(username):
+    server = ldap3.Server(LDAP_SERVER, use_ssl=True, get_info=ldap3.ALL)
+    with ldap3.Connection(server,
+                          user=LDAP_USER,
+                          password=LDAP_PASS,
+                          auto_bind=True) as conn:
+        if conn.bind():
+            if conn.search('DC=stadt,DC=worms', "(&(sAMAccountName=" + username + "))",
+                               ldap3.SUBTREE,
+                               attributes=['mail', 'memberOf', 'department', 'cn']):
+                cn = conn.entries[0]['cn']
+                mail = conn.entries[0]['mail']
+                department = conn.entries[0]['department']
+                groups = [group.split(',')[0].split('=')[1] for group in conn.entries[0]['memberOf']]
+                return cn, mail, department, groups
+    return ""
 
 
 class Entry(db.Model):
@@ -49,11 +105,11 @@ def login():
         return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        user = authenticate_ldap(form.username.data, form.password.data)
+        if user:
             login_user(user, remember=True)
-            flash("Eingeloggt als " + user.username + "!", "success")
-            return redirect(url_for("home"))
+            flash("Eingeloggt als " + user.id + "!", "success")
+            return redirect(url_for('home'))
         else:
             flash("Falsches Passwort oder Benutzername", "danger")
     return render_template("login.html", title="login", form=form)
@@ -65,31 +121,46 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/save", methods=["POST"])
+@app.route("/save", methods=["POST", "PUT"])
 def save():
     if current_user.is_authenticated:
         if request.method == 'POST':
             content = json.loads(request.data)
-            db.session.add(Entry(data=content, user=current_user.email))
+            db.session.add(Entry(data=content, user=current_user.id))
             db.session.commit()
-            # for data in content:
-            #     if "Text" in data.keys():
-            #         print(data["Text"])
-            #     else:
-            #         filename = "temp/"+data["code"]+".png"
-            #         try:
-            #             with open(filename, "wb") as fh:
-            #                 # Change imagevalue to string and split the b64 coding, then decode with b64, safe to image
-            #                 fh.write(base64.b64decode((str(data["img"]).split(",")[1].encode("ascii")), validate=True))
-            #         except Exception as e:
-            #             print(e)
+            return "Success", 200
+        elif request.method == 'PUT':
+            entries = db.session.query(Entry).filter(Entry.user == current_user.id).all()
+            for entry in entries:
+                print(entry.id)
             return "Success", 200
         else:
-            print("no POST")
+            print("no POST or PUT")
     else:
         print("no authenticated user")
 
 
+
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
-    #app.run(host='0.0.0.0', port=3000, debug=True, ssl_context="adhoc")
+    # No SSL
+    # app.run(host='0.0.0.0', port=3000, debug=True)
+
+    # With SSL active, for testing purposes on iPad for ex.
+    app.run(host='0.0.0.0', port=3000, debug=True, ssl_context="adhoc")
+
+
+
+# Old part for converting base64 string to image
+# for data in content:
+#     if "Text" in data.keys():
+#         print(data["Text"])
+#     else:
+#         filename = "temp/"+data["code"]+".png"
+#         try:
+#             with open(filename, "wb") as fh:
+#                 # Change imagevalue to string and split the b64 coding, then decode with b64, safe to image
+#                 fh.write(base64.b64decode((str(data["img"]).split(",")[1].encode("ascii")), validate=True))
+#         except Exception as e:
+#             print(e)

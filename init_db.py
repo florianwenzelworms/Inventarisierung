6d603c509a2b8eb5639dd3c6b7ee063749068ebd
wenzelf <florian.wenzel@worms.de>
2024-08-15 13:00:08 +0200

    Reformatting

--- init_db.py
+++ init_db.py
@@ -3,7 +3,3 @@ from app import app, db, User
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        #db.session.add(User(id=1, username="wenzelf", email="florian.wenzel@worms.de", password="Fjbteam123"))
-        #db.session.commit()
-
-
